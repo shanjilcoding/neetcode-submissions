363c112e30e27defa60e9614a9cf339fd2303dd6
Shanjil Hassan <shanjilcoding@gmail.com>
2026-08-26 00:02:21 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 3.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "String"
+print(type(variable))
+variable = [1, 2, 3]
+print(type(variable))
