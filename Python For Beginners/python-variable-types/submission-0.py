a8935ceef795ae494ed40c024632fa7f61ce2534
Shanjil Hassan <shanjilcoding@gmail.com>
2026-08-26 00:00:52 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+age = 25             
+temperature = 98.6   
+is_true = True       
+name = "Alice"       
+my_list = [1, 2, 3] 
+
+
+
+
+print(type(age))
+print(type(temperature))
+print(type(is_true))
+print(type(name))
+print(type(my_list))
